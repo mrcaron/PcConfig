81467acb1f92063ea88fd791d2290cf2b8ccc5a0
Michael Caron <michael.caron@3ds.com>
2021-04-05 12:39:06 -0400

    removing posh-git in favor of oh-my-posh

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,3 @@
-﻿
-Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
-
 ~\Documents\WindowsPowerShell\Scripts\Invoke-Batchfile.ps1
 
 # load VS 2019 into environment
@@ -10,3 +7,9 @@ function global:Invoke-VS2019Env {
 function global:Invoke-VS2017Env {
     invoke-batchfile "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 }
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+Invoke-Expression (oh-my-posh --init --shell pwsh)`n
