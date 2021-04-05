802b786db00e408671efa6a077aece02ee4875cd
Michael Caron <michael.caron@3ds.com>
2021-04-05 13:14:12 -0400

    adds pwsh prompt

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+~\Documents\WindowsPowerShell\Scripts\Invoke-Batchfile.ps1
+
+# load VS 2019 into environment
+function global:Invoke-VS2019Env {
+    invoke-batchfile "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+}
+function global:Invoke-VS2017Env {
+    invoke-batchfile "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+}
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/jandedobbeleer.omp.json")
